df08d9ddc903938e4ef397879067ccd8cba6bf77
Darlene Zouras <dzouras@gmail.com>
2017-04-07 21:51:00 -0500

    continuing work on female face offerings

--- src/routes/faces.js
+++ src/routes/faces.js
@@ -12,7 +12,57 @@ exports.faceStyles = function(req, res) {
             {name: "Defiant Face", image: "defiantface1"},
             {name: "Prudent Face", image: "prudentface1"},
             {name: "Intelligent Face", image: "intelligentface1"},
-            {name: "Strong Stare", image: "strongstare1"}
+            {name: "Strong Stare", image: "strongstare1"},
+            {name: "Angel Glow", image: "angelglow1"},
+            {name: "Babyface Pout", image: "babyfacepout1"},
+            {name: "Pucker Up Face", image: "puckerupface1"},
+            {name: "Dollface Look", image: "dollfacelook1"},
+            {name: "Hopeless Gaze", image: "hopelessgaze1"},
+            {name: "Soul's Window", image: "soulswindow1"},
+            {name: "Curious Look", image: "curiouslook1"},
+            {name: "Wide-eyed Girl", image: "wideeyedgirl1"},
+            {name: "Innocent Look", image: "innocentlook1"},
+            {name: "Tender Love", image: "tenderlove1"}
+        ],
+        nlcF: [
+            {name: "Beauty Stare", image: "beautystare1"},
+            {name: "Prudent Face", image: "prudentface1"},
+            {name: "Intelligent Face", image: "intelligentface1"},
+            {name: "Strong Stare", image: "strongstare1"},
+            {name: "Angel Glow", image: "angelglow1"},
+            {name: "Babyface Pout", image: "babyfacepout1"},
+            {name: "Pucker Up Face", image: "puckerupface1"},
+            {name: "Hopeless Gaze", image: "hopelessgaze1"},
+            {name: "Soul's Window", image: "soulswindow1"}
+        ],
+        orbisF: [
+            {name: "Defiant Face", image: "defiantface1"},
+            {name: "Prudent Face", image: "prudentface1"},
+            {name: "Intelligent Face", image: "intelligentface1"},
+            {name: "Strong Stare", image: "strongstare1"},
+            {name: "Angel Glow", image: "angelglow1"},
+            {name: "Babyface Pout", image: "babyfacepout1"},
+            {name: "Pucker Up Face", image: "puckerupface1"},
+            {name: "Dollface Look", image: "dollfacelook1"},
+            {name: "Hopeless Gaze", image: "hopelessgaze1"},
+            {name: "Soul's Window", image: "soulswindow1"},
+            {name: "Curious Look", image: "curiouslook1"},
+            {name: "Innocent Look", image: "innocentlook1"},
+            {name: "Tender Love", image: "tenderlove1"}
+        ],
+        ludiF: [
+            {name: "Defiant Face", image: "defiantface1"},
+            {name: "Prudent Face", image: "prudentface1"},
+            {name: "Intelligent Face", image: "intelligentface1"},
+            {name: "Strong Stare", image: "strongstare1"},
+            {name: "Angel Glow", image: "angelglow1"},
+            {name: "Babyface Pout", image: "babyfacepout1"},
+            {name: "Pucker Up Face", image: "puckerupface1"},
+            {name: "Dollface Look", image: "dollfacelook1"},
+            {name: "Hopeless Gaze", image: "hopelessgaze1"},
+            {name: "Soul's Window", image: "soulswindow1"},
+            {name: "Curious Look", image: "curiouslook1"},
+            {name: "Wisdom Glance", image: "wisdomgaze1"}
         ]
     };
     context.title = "MapleNow - Plastic Surgery";
